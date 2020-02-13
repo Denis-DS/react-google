ec18a2e7fef4e38036269fb2d68290a2230cb425
Denis-DS <56930333+Denis-DS@users.noreply.github.com>
2020-02-13 18:27:52 +0200

    first commit

---
+++ build/precache-manifest.4d2e0aca15e8efcaf1b382379740eb89.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "86ee0695ab8b9f22e1e7e3e8773c00e8",
+    "url": "./index.html"
+  },
+  {
+    "revision": "264ce088128dc29c392a",
+    "url": "./static/css/main.47e0978f.chunk.css"
+  },
+  {
+    "revision": "9276fda58bda3d91d0d3",
+    "url": "./static/js/2.8602913b.chunk.js"
+  },
+  {
+    "revision": "99bd0487192ec9e7d9ee8fbbd91ee444",
+    "url": "./static/js/2.8602913b.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "264ce088128dc29c392a",
+    "url": "./static/js/main.8f9e0a0b.chunk.js"
+  },
+  {
+    "revision": "f9a752b41214259e92c4",
+    "url": "./static/js/runtime-main.39dcb93e.js"
+  }
+]);
